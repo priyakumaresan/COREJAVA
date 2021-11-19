852bcc83886cea97d71cc127eaed6bfe837c435f
priya.k <rocksrekshi77@gmail.com>
2021-11-19 12:00:47 +0530

    Commit for changing in Honda file

--- src/Abstractclass/Honda4.java
+++ src/Abstractclass/Honda4.java
@@ -5,7 +5,8 @@ abstract class Bike {
 }
 class Honda4 extends Bike{
 	void run() {
-		System.out.println("Running safely");
+		System.out.println("Running safely...");
+		System.out.println("Execution in run method");
 	}
 public static void main(String[] args) {
 		Honda4 obj=new Honda4();
